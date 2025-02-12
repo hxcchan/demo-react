cc927d6f34e6bbbef0b7f989263caeb34d5bc5ed
Song Han <song.han1024@gmail.com>
2025-02-12 04:03:52 -0500

    use react-player to add YouTube video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import {useState} from "react";
 import './App.css';
+import ReactPlayer from "react-player/youtube";
 
 function App() {
     const [text, setText] = useState("Song")
@@ -11,6 +12,7 @@ function App() {
                 e => setText(e.currentTarget.value)
             }/>
             <button type={"submit"}>Submit</button>
+            <ReactPlayer url="https://www.youtube.com/watch?v=I3OJUwILelU" controls={true} />
         </div>
     );
 }
